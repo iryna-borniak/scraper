1555b0e95b3dc82278d7324c61c048c81b1d91b0
Iryna Borniak <la.aerea.1@gmail.com>
2024-02-12 04:19:11 +0200

    add translation

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -53,7 +53,7 @@ SimpleForm.setup do |config|
 
     ## Inputs
     # b.use :label_input
-    b.use :label
+    # b.use :label
     b.use :input, class: "input", error_class: "is-invalid", valid_class: "is-valid"
     b.use :hint,  wrap_with: { tag: :span, class: :hint }
     b.use :error, wrap_with: { tag: :span, class: :error }
